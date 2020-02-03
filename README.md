ae5bff82b09cf020f129c8704c80d0062ee091b0
Jeyhun Jeyhunzade <ceyhun.zade1998@gmail.com>
2020-02-04 01:46:26 +0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# MrJeyhunPortfolio
-Products of my coding love :)<br />
-<a href="https://mrjeyhun.github.io/MrJeyhunPortfolio/">MrJeyhun Portfolio</a>
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MrJeyhunPortfolio/precache-manifest.d0842b9c13a7a5313f3dc55b1cbc0358.js"
+  "/precache-manifest.c02074fee19a2f0436c33a307ca07d6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MrJeyhunPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
